0ff4013187629540763b48b58894a52058e7d772
Freddie Hoy <freddiehoy@hotmail.com>
2020-04-23 09:24:46 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-1',
+    BUCKET: 'notes-app-uploading-tutorial'
+  },
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://286ejbvc3a.execute-api.us-east-1.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_SLTRADLhQ',
+    APP_CLIENT_ID: '3s0vhoetevd8qi0nuvk8oj9odl',
+    IDENTITY_POOL_ID: 'us-east-1:8e8ce459-5c2c-4672-b57f-cad1dbeacf1f'
+  }
+};
